3f577907e941e8e8b6002fdb9d166039f17aac0e
Sofía Carnevale <carnevalebsofia@gmail.com>
2023-06-13 15:55:23 -0300

    initial commit

---
+++ PokemonReviewApp/Models/Pokemon.cs
@@ -0,0 +1,13 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }   
+        public DateTime Birth { get; set; }
+        public ICollection<Review> Reviews { get; set; }
+        public ICollection<PokemonOwner> PokemonOwner { get; set; }
+        public ICollection<PokemonCategory> PokemonCategory { get; set; } 
+
+    }
+}
---
+++ PokemonReviewApp/Models/Reviewer.cs
@@ -0,0 +1,11 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Reviewer
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ICollection<Review> Reviews { get; set;}
+
+    }
+}
